bc1c943b7b38fdb2f9e15c7d38c1bd8c478ddfa8
OleksiiKhorchev <Oleksii.Khorchev@gmail.com>
2014-08-09 14:11:12 +0300

    added case for plus/minus tasks

--- stresscalc/MainWindow.xaml.cs
+++ stresscalc/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -15,6 +16,25 @@ namespace stresscalc
         }
 
         private int _preresult;
+
+        private enum Sign
+        {
+            Null,
+
+            [Description("+")]
+            Plus,
+
+            [Description("-")]
+            Minus,
+
+            [Description("*")]
+            Multiply,
+
+            [Description("/")]
+            Divide
+        }
+
+        private Sign _sign;
 
         private void Number_Click(object sender, RoutedEventArgs e)
         {
@@ -34,16 +54,52 @@ namespace stresscalc
         private void plus_Click(object sender, RoutedEventArgs e)
         {
             
+            //trace.Content = trace.Content + "+";
+            //_preresult = _preresult + Convert.ToInt32(screen.Text);
+            //screen.Text = "";
+            //showme.Content = Convert.ToString(_preresult);
+
+            // TODO: Make work for other signs - i.e. plus, multilpy, divide
+            if (_sign == Sign.Plus)
+                _preresult = _preresult + Convert.ToInt32(screen.Text);
+            else
+                _preresult = Convert.ToInt32(screen.Text);
+
+            _sign = Sign.Plus;
+
+
             trace.Content = trace.Content + "+";
-            _preresult = _preresult + Convert.ToInt32(screen.Text);
             screen.Text = "";
-            showme.Content = Convert.ToString(_preresult);
-            
+            showme.Content = _preresult;
         }
 
         private void minus_Click(object sender, RoutedEventArgs e)
         {
-            
+            // TODO: Make work for other signs - i.e. plus, multilpy, divide
+            if (_sign != Sign.Null)
+                MakeCount();
+            else
+                _preresult = Convert.ToInt32(screen.Text);
+
+            _sign = Sign.Minus;
+
+
+            trace.Content = trace.Content + "-";
+            screen.Text = "";
+            showme.Content = _preresult;
+        }
+
+        private void MakeCount()
+        {
+            switch (_sign)
+            {
+                case Sign.Minus:
+                    _preresult = _preresult - Convert.ToInt32(screen.Text);
+                    break;
+                case Sign.Plus:
+                    _preresult = _preresult + Convert.ToInt32(screen.Text);
+                    break;
+            }
             
         }
     }
