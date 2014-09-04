85067aa4853d90dfd0289435b7cb0aa5812eff8a
Bohdan <te.dark@mail.ru>
2014-09-04 19:32:43 +0300

    simple mvc calculator added

---
+++ MvcCalculator/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCalculator.Models;
+
+namespace MvcCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(CalcModel model)
+        {
+            string result = string.Empty;
+            switch (model.Action)
+            {
+                case "+":
+                    result = (model.FirstNumber + model.SecondNumber).ToString(CultureInfo.InvariantCulture);
+                    break;
+                case "-":
+                    result = (model.FirstNumber - model.SecondNumber).ToString(CultureInfo.InvariantCulture);
+                    break;
+                case "*":
+                    result = (model.FirstNumber * model.SecondNumber).ToString(CultureInfo.InvariantCulture);
+                    break;
+                case "/":
+                    result = (model.FirstNumber / model.SecondNumber).ToString(CultureInfo.InvariantCulture);
+                    break;
+            }
+
+            ViewBag.Result = result;
+
+            
+            return View();
+        }
+
+    }
+}
---
+++ MvcCalculator/Models/CalcModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcCalculator.Models
+{
+    public class CalcModel
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        public string Action { get; set; }
+    }
+}
